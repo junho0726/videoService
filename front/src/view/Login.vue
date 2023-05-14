ff3a16f6bb27581bf5dbc3929e8d7ee43a0a0ea5
IWBTM <rnlduadl2000@naver.com>
2023-05-15 00:11:53 +0900

    회원ê°로그인 그 validation 처리

--- front/src/view/Login.vue
+++ front/src/view/Login.vue
@@ -6,9 +6,13 @@
     <div>
         <input type="text" id="id" v-model="id" placeholder="아이디를 입력해주세요.">
     </div>
+    <span>{{ checkEmptyId }}</span>
+    <br>
     <div>
         <input type="text" id="pw" v-model="pw" placeholder="비밀번호를 입력해주세요.">
     </div>
+    <span>{{ checkEmptyPw }}</span>
+    <br>
     <div>
         <button type="button" @click="login">Login</button>
         <button type="button">Join</button>
@@ -23,17 +27,40 @@ import { ref } from 'vue';
 let id = ref('');
 let pw = ref('');
 
+let checkEmptyId = ref('');
+let checkEmptyPw = ref('');
+
 let login = async () => {
-    try {
-        let response = await axios.post('/api/login', {
-            id: id.value,
-            pw: pw.value
-        });
-        console.log(response.data);
-    } catch (error) {
-        console.error(error);
+
+    if(checkEmpty()) {
+
+        try {
+            let response = await axios.post('/api/login', {
+                id: id.value,
+                pw: pw.value
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
     }
 };
+
+function checkEmpty() {
+    if(id.value == '') {
+        checkEmptyId.value = '아이디를 입력해주세요.';
+        return false;
+    } else {
+        checkEmptyId.value = '';
+    }
+    if(pw.value == '') {
+        checkEmptyPw.value = '비밀번호를 입력해주세요.';
+        return false;
+    } else {
+        checkEmptyPw.value = '';
+    }
+    return true;
+}
 
 </script>
 
