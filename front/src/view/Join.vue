1b62b04d186c065b94743cf1eba16f736a0d7890
IWBTM <rnlduadl2000@naver.com>
2023-05-14 23:08:44 +0900

    회우ë원 가입 로직 í구현

--- front/src/view/Join.vue
+++ front/src/view/Join.vue
@@ -4,36 +4,58 @@
     <h1>Join 페이지</h1>
 
     <div>
-        <input type="text" id="id" name="id" placeholder="아이디를 입력해주세요.">
+        <input type="text" id="id" v-model="id" placeholder="아이디를 입력해주세요.">
     </div>
     <div>
-        <input type="text" id="pw" name="pw" placeholder="비밀번호를 입력해주세요.">
+        <input type="text" id="pw" v-model="pw" placeholder="비밀번호를 입력해주세요.">
     </div>
     <div>
-        <input type="text" id="re-pw" name="re-pw" placeholder="비밀번호를 한번 더 입력해주세요.">
+        <input type="text" id="re-pw" v-model="rePw" placeholder="비밀번호를 한번 더 입력해주세요.">
     </div>
     <div>
-        <input type="text" id="name" name="name" placeholder="이름을 입력해주세요.">
+        <input type="text" id="name" v-model="name" placeholder="이름을 입력해주세요.">
     </div>
     <div>
-        <input type="text" id="email" name="email" placeholder="이메일을 입력해주세요.">
+        <input type="text" id="email" v-model="email" placeholder="이메일을 입력해주세요.">
     </div>
     <div>
-        <input type="text" id="tel" name="tel" placeholder="연락처를 입력해주세요.">
+        <input type="text" id="tel" v-model="tel" placeholder="연락처를 입력해주세요.">
     </div>
     <div>
         <button type="button">Login</button>
-        <button type="button">Join</button>
+        <button type="button" @click="change()">Join</button>
     </div>
 </template>
 
 <script setup>
 import Header from "@/layout/Header.vue";
 import axios from "axios";
+import { ref } from 'vue';
 
-axios.post('/api/join', {
+let id = ref('');
+let pw = ref('');
+let rePw = ref('');
+let name = ref('');
+let email = ref('');
+let tel = ref('');
 
-})
+
+
+let join() = async () => {
+    try {
+        let response = await axios.post('/api/joinProc', {
+            id: id.value,
+            pw: pw.value,
+            rePw: rePw.value,
+            name: name.value,
+            email: email.value,
+            tel: tel.value
+        });
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 </script>
 
