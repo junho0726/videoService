90727d7192e2b511415c8015a1edd2df0a875c42
IWBTM <rnlduadl2000@naver.com>
2023-05-17 00:17:58 +0900

    channel, mypage 추가

--- front/src/user/MyChannel.vue
+++ front/src/user/MyChannel.vue
@@ -1,9 +1,24 @@
 <template>
+    <div>
+        <Header @show-sidebar="showSidebar()"/>
+        <div class="content">
+            <SideBar v-if="isShowSidebar"/>
+            내 채널
+        </div>
+    </div>
 
 </template>
 
 <script setup>
+import Header from '@/layout/Header.vue';
+import SideBar from '@/layout/SideBar.vue';
+import { ref } from 'vue';
 
+let isShowSidebar = ref(false);
+
+function showSidebar() {
+    isShowSidebar.value = !isShowSidebar.value;
+}
 </script>
 
 <style>
