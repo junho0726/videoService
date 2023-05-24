6382fc84763e4e0e1207bc5bf9519c385f388581
IWBTM <rnlduadl2000@naver.com>
2023-05-24 18:59:06 +0900

    File TEST 중

--- src/main/java/com/video/service/config/WebConfig.java
+++ src/main/java/com/video/service/config/WebConfig.java
@@ -1,7 +1,9 @@
-/*package com.video.service.config;
+package com.video.service.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,9 +13,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // /api로 들어오는 모든 url 허용
                 .allowedOrigins("http://localhost:8081") // 해당 url에서의 접근 허용
-                .allowedMethods("GET", "POST") // 해당 REST API로의 접근 허용
-                .allowedHeaders("*"); // 모든 헤더 허용
+                .allowedMethods("GET", "POST");
 
         // 위 설정들이 모두 true일 때만 접근 허용됨
     }
-}*/
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.noCache());
+    }
+}
